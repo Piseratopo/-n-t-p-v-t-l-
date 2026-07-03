2b314b1558c268c924f2f56f2f8bb657fdc55c7b
Piseratopo <buinhatminh2k52005@gmail.com>
2026-07-03 10:55:10 +0700

    chuẩn bị lại cho phần lô-gích

--- utils/test.py
+++ utils/test.py
@@ -3,5 +3,5 @@ from util_latex_general import parse_expression_with_parentheses
 
 latex_output_file = "output.tex"
 
-write_logic_table_latex(latex_output_file, r"(P <=> Q & R) & (R => Q) => (P <=> Q)")
+write_logic_table_latex(latex_output_file, r"P => Q <=> R")
 
